44f455dea536c74f38ffd7f443c0377237ec1526
RukeshB <rukeshbasukala@gmail.com>
2023-09-15 10:19:58 +0545

    location validation test

---
+++ spec/models/location_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Location, type: :model do
+  let(:location) { create(:location) }
+
+  describe 'Valid' do
+    it 'all data present' do
+      expect(location).to(be_valid)
+    end
+  end
+
+  describe 'InValid' do
+    it 'City not Present' do
+      location.city = nil
+      expect(location).not_to(be_valid)
+    end
+
+    it 'Country not Present' do
+      location.country = nil
+      expect(location).not_to(be_valid)
+    end
+  end
+end
